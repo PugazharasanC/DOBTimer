d1f04d604cc69f0e48c9fd79ea0eec5652fbcfe6
PugazharasanC <32019083+PugazharasanC@users.noreply.github.com>
2020-04-28 13:16:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,12 +6,23 @@ function getTimer() {
         console.log(diff);
         if (diff > 0) {
             var days = Math.floor(diff / (3600000 * 24));
-            var hrs = Math.floor((diff % (3600000 * 24)) / (3600000));
-            var mins = Math.floor((diff % (3600000)) / (60000));
-            var secs = Math.floor((diff % (60000)) / 1000);
-            document.getElementById('timer').innerHTML = days + ' Days ' + hrs + ' Hours ' + mins + ' Mins ' + secs + ' Secs';
+            var year = new Date().getFullYear() - dob.getFullYear();
+            var month = new Date().getMonth() - dob.getMonth();
+            var hrs = Math.floor((diff) / (3600000));
+            var mins = Math.floor(diff / (60000));
+            var secs = Math.floor(diff / 1000);
+            if(month<0){
+                year-=1;
+                month = 12*year + 12+month;
+            }
+            document.getElementById('year').innerHTML = year + 'Years';
+            document.getElementById('month').innerHTML = month + 'Month'
+            document.getElementById('days').innerHTML = days + 'Days'
+            document.getElementById('hours').innerHTML = hrs + 'Hrs'
+            document.getElementById('mins').innerHTML = mins + 'Mins'
+            document.getElementById('secs').innerHTML = secs + ' Secs';
         } else {
-            document.getElementById('timer').innerHTML = 'You\'re not yet born';
+            document.getElementById('year').innerHTML = 'You\'re not yet born';
         }
     }, 1000)
 }
